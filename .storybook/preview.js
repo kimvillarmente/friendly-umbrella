9353c190302fd568a28fe970233705265ec1b577
Kimberly <you@example.com>
2021-01-25 22:51:37 +0800

    put comment

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,63 +1,59 @@
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 }
 
-import React from "react";
+import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 
 export const decorators = [
   (Story) => (
-    <ThemeProvider theme={{
-      colors: {
-        text: '#000',
-        background: '#fff',
-        primary: '#07c',
-        secondary: '#05a',
-        accent: '#609',
-        muted: '#f6f6f6f',
-      },
-      fonts: {
-        body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
-        heading: 'Georgia, serif',
-        monospace: 'Menlo, monospace',
-      },
-      fontSizes: [
-        12, 14, 16, 20, 24, 32, 48, 64
-      ],
-      fontWeights: {
-        body: 400,
-        heading: 700,
-        bold: 700,
-      },
-      lineHeights: {
-        body: 1.5,
-        heading: 1.125,
-      },
-      letterSpacings: {
-        body: 'normal',
-        caps: '0.2em',
-      },
-      buttons: {
-        primary: {
-          color: 'background',
-          bg: 'primary',
+    <ThemeProvider
+      theme={{
+        colors: {
+          text: '#000',
+          background: '#fff', // Canvas Background
+          primary: '#07c', // primary button color
+          secondary: '#05a',
+          accent: '#609',
+          muted: '#f6f6f6f',
         },
-        secondary: {
-          color: 'background',
-          bg: 'secondary',
+        fonts: {
+          body:
+            'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+          heading: 'Georgia, serif',
+          monospace: 'Menlo, monospace',
         },
-        gray: {
-          color: 'background',
-          bg: 'gray',
+        fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+        fontWeights: {
+          body: 400,
+          heading: 700,
+          bold: 700,
         },
-      },
-    }}>
+        lineHeights: {
+          body: 1.5,
+          heading: 1.125,
+        },
+        letterSpacings: {
+          body: 'normal',
+          caps: '0.2em',
+        },
+        buttons: {
+          primary: {
+            color: 'background',
+            bg: 'primary',
+          },
+          secondary: {
+            color: 'background',
+            bg: 'secondary',
+          },
+          gray: {
+            color: 'background',
+            bg: 'gray',
+          },
+        },
+      }}
+    >
       <Story />
     </ThemeProvider>
   ),
-];
-
-
-
-
+]
--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,36 +1,43 @@
-import React from 'react';
+import React from 'react'
 
-import { Button } from '../components/Button';
+import { Button } from '../components/Button'
 
 export default {
   title: 'Example/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: 'color' }, // controls > backgroundColor name
   },
-};
+}
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
+// Primary — List of Name under Example > Button
+export const Primary = Template.bind({})
 Primary.args = {
-  variant: 'primary',
-  children: 'Button',
-};
+  variant: 'primary', // controls > variant placeholder
+  children: 'Button', // controls > children placeholder
+}
 
-export const Secondary = Template.bind({});
+export const Secondary = Template.bind({})
 Secondary.args = {
   label: 'Button',
-};
+}
 
-export const Large = Template.bind({});
+export const Large = Template.bind({})
 Large.args = {
   size: 'large',
   label: 'Button',
-};
+}
 
-export const Small = Template.bind({});
+export const Small = Template.bind({})
 Small.args = {
   size: 'small',
   label: 'Button',
-};
+}
+
+export const Tiny = Template.bind({})
+Tiny.args = {
+  variant: 'tiny', // controls > variant placeholder
+  children: 'Button', // controls > children placeholder
+}
